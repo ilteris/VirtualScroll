5eb6d3625e1216783200562674244667765fa29c
ilteris <ilteris@gmail.com>
2013-10-21 21:45:24 -0400

    add viewcontrollers and scrollview

---
+++ VirtualScroll/IKVirtualScrollViewController.m
@@ -0,0 +1,30 @@
+//
+//  IKViewController.m
+//  VirtualScroll
+//
+//  Created by ilteris on 10/21/13.
+//  Copyright (c) 2013 ilteris. All rights reserved.
+//
+
+#import "IKVirtualScrollViewController.h"
+
+@interface IKVirtualScrollViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
+
+@implementation IKVirtualScrollViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
